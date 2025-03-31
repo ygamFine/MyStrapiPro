1b380a92ba3171269dcc47252ca791e95efd94b5
yun.zhang <ygamfine@126.com>
2025-03-31 19:20:42 +0800

    mdoify config

---
+++ src/admin/app.tsx
@@ -0,0 +1,15 @@
+export default {
+    config: {
+      locales: ['zh-Hans'], // 允许使用的语言
+      translations: {
+        'zh-Hans': {
+          'Auth.form.email.label': '电子邮件',
+          Users: '用户',
+          City: '城市',
+          Id: '编号',
+        },
+      },
+    },
+    bootstrap() {},
+  };
+  
