ac481386c2b1a9b5c10a2574b7ea56d2c066f3cc
yun.zhang <ygamfine@126.com>
2025-05-06 09:45:31 +0800

    save env code

---
+++ dist/src/api/menu/controllers/menu.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * menu controller
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreController('api::menu.menu');
